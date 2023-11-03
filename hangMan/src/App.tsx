0a5148562b24e4d52ab9ec77b1cbba8aeeb7ec32
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-11-03 18:55:45 +0100

    testing HangmanDrawing component

--- hangMan/src/App.tsx
+++ hangMan/src/App.tsx
@@ -1,6 +1,9 @@
 import { useState } from 'react';
 import words from './wordList.json';
 import './App.css';
+import HangmanDrawing from './components/HangmanDrawing';
+import HangmanWord from './components/HangmanWord';
+import Keyboard from './components/Keyboard';
 function App() {
   const [wordToGuesse, setWordToGuesse] = useState(() => {
     return words[Math.floor(Math.random() * words.length)];
@@ -13,6 +16,9 @@ function App() {
     <>
       <div className="app">
         <div className="result">Lose Win</div>
+        <HangmanDrawing />
+        <HangmanWord />
+        <Keyboard />
       </div>
     </>
   );
